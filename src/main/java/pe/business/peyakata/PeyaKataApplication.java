4c2c4436d9d2a906b4bc815c5082d224b2f0245a
Fernando Supo <fernando.supo6@gmail.com>
2021-06-23 22:48:39 -0500

    init commit

---
+++ src/main/java/pe/business/peyakata/PeyaKataApplication.java
@@ -0,0 +1,13 @@
+package pe.business.peyakata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeyaKataApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PeyaKataApplication.class, args);
+  }
+
+}
